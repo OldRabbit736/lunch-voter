7b3f0c1519c7af4db4d11385bc8eb1d0ffe86e29
OldRabbit736 <36852907+OldRabbit736@users.noreply.github.com>
2019-05-21 23:27:54 +0900

    del btn and ionicons

--- src/components/Results/Votes/Vote/Vote.js
+++ src/components/Results/Votes/Vote/Vote.js
@@ -6,7 +6,7 @@ const vote = (props) => {
         <div className={classes.Vote}>
             <label className={classes.label}>{props.vote.name}</label>
             <label className={classes.label}>{props.vote.time}</label>
-            <button id={props.id} onClick={props.onClicked}>del</button>
+            <button className={classes.delBtn} id={props.id} onClick={props.onClicked}>del</button>
             {props.vote.picks.map((pick, index) => {
                 return (
                     <h3 className={classes.h3} key={index}>{index+1}픽 {pick}</h3>                        
