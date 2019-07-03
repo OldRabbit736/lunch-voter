4ce8c0b94298de17db4a5ebe1c7a1dadfe17769e
OldRabbit736 <36852907+OldRabbit736@users.noreply.github.com>
2019-07-03 21:40:40 +0900

    약간 수정 및 deploy

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -6,7 +6,7 @@ const selector = (props) => {
     return (
         <div className={classes.Selector}>
             <h2 className={classes.h2}>Your Pick</h2>
-            <label className={classes.label}>~사용법: 가게 이름을 찍으세요~</label>
+            <label className={classes.label}>리스트에서 가게를 선택하세요</label>
             <div>
                 <label className={classes.label2}>제출자</label>
                 <input
@@ -24,10 +24,10 @@ const selector = (props) => {
                 </input>
             </div>
             
+            <button className={classes.btnConfirm} onClick={props.storeConfirm}>이 픽으로 간다!</button>
+            <div className={classes.padding}></div>
             <button className={classes.btn} onClick={props.storeReset}>픽 리셋</button>
-            <button className={classes.btn} onClick={props.selectRandomly}>랜덤3</button>
-            <button className={classes.btn} onClick={props.storeConfirm}>이 픽으로 간다!</button>
-
+            <button className={classes.btn} onClick={props.selectRandomly}>랜덤3</button>            
             <div>
                 {props.selectedStores.map((store, index) => {
                     return (
@@ -37,6 +37,8 @@ const selector = (props) => {
                     )
                 })}
             </div>
+
+            
         </div>
     );
 }
